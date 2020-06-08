dba9f3307d2acba7acf70bcceeee87c2e82716b6
1onghoang <46763078+1onghoang@users.noreply.github.com>
2020-06-07 21:51:26 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://long:cQv7zBkQpCJBBqGI@cluster0-y337p.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "YOUR MONGO URI HERE",
   secretOrKey: "secret"
 };
